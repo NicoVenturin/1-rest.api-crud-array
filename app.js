05629cb27cb71dd54fb0e2b734218788e5f54ac4
NicoVenturin <145157147+NicoVenturin@users.noreply.github.com>
2024-09-02 20:25:37 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -18,16 +18,47 @@ app.post('/productos', (req,res) =>{
     productos.push(nuevoproducto);
     //res.send(`mandando nuevo producto`);
 });
-app.put('/productos', (req,res) =>{
-    
-    
-    res.send(`actualizando un producto`);
+app.put('/productos/:id', (req,res) =>{
+    const prodEncontrado = productos.find((p)=>p.id == req.params.id);
+    if(!prodEncontrado){
+        return res.status(404),res.json("no se encuentra el producto");
+    }
+    console.log(req.params.id);
+    console.log(req.body);
+    const nuevosDatos = req.body;
+    //const x = (condicion) ?(if true):(else)
+    productos = productos.map(p=>p.id==req.params.id ? {...p,...nuevosDatos} : p)
+    res.json("prod actualizado");
+    //res.send(`actualizando un producto`);
 });
-app.delete('/productos', (req,res) =>{
-    res.send(`borrando un producto`);
+app.delete('/productos/:id', (req,res) =>{
+    //res.send(`borrando un producto`);
+    const prodEncontrado = productos.find((p)=>p.id == req.params.id);
+    if(!prodEncontrado){
+        return res.status(404),res.json("no se encuentra el producto");
+    }
+    //filter crea un reemplazo del array sin el borrado
+    productos = productos.filter((p)=>p.id != req.params.id);
+    console.log(productos);
+    res.json("producto eliminado");
 });
-app.get('/productos:id', (req,res) =>{
-    res.send(`mostrando un producto`);
+app.get('/productos/:id', (req,res) =>{
+    console.log(req.params.id);
+    //formato largo:
+    //const prodEncontrado=productos.find((producto)=>{
+    //    return producto.id == req.params.id;
+    //});
+    //formato corto:
+    const prodEncontrado = productos.find((p)=>p.id == req.params.id);
+    if(!prodEncontrado){
+        res.json("no se encuentra el producto");
+    }
+    else{
+        res.json({"mensaje":"producto encontrado","producto":prodEncontrado});
+    }
+    
+
+
 });
 app.listen(port, ()=>{
     console.log(`Servidor corriendo en el puerto ${port}`);
